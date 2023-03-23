5d10114d05e930d24c8daf5775a026a82aa78710
barrajac <108970552+barrajac@users.noreply.github.com>
2023-03-22 23:31:19 -0700

    update4 commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//1. cached my elements into variables
 let playerScore = 0; //had to chng const to let
 let computerScore = 0;
 const playerScoreSpan = document.querySelector("#player-score");
@@ -11,15 +12,90 @@ const paperDiv = document.querySelector("#paper");
 const scissorsDiv = document.querySelector("#scissors");
 const choicesArray = ["rock", "paper", "scissors"];
 
-// Generate random RPS choice for Computer
+function capFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+//3. Generate random RPS choice for Computer
 function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * choicesArray.length); //array has 3 elements
   let randomChoice = choicesArray[randomIndex];
+  addComputerChoiceImg(randomChoice);
   return randomChoice;
 }
 // console.log(getComputerChoice());  //checkpoint: random choice on console
 
-// fxn that stores corresponding img to playerchoice
+//7. what happens when we win, lose, or draw per round, user score should increment
+function ganar(playerChoice, computerChoice) {
+  playerScore++; //increment winner score
+  playerScoreSpan.innerHTML = playerScore; //update scoreboard
+  computerScoreSpan.innerHTML = computerScore; //update scoreboard
+  playerChoice = capFirstLetter(playerChoice); //cap first letter of RPS for player
+  computerChoice = capFirstLetter(computerChoice); //cap first letter of RPS for computer
+  outcomeMsg.innerHTML = `${playerChoice} beats ${computerChoice}. Point for you!`;
+  //   console.log("Player won");
+  //   console.log(playerScore);
+}
+
+function perder(playerChoice, computerChoice) {
+  console.log("Player lost");
+}
+
+function empate(playerChoice, computerChoice) {
+  console.log("Draw");
+}
+
+//4. create the game fxn that carries out RPS play
+function rpsGame(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "rockscissors":
+    case "paperrock":
+    case "scissorspaper":
+      ganar(userChoice, computerChoice);
+      //   console.log("PLAYER WINS!");
+      break;
+    case "scissorsrock":
+    case "rockpaper":
+    case "paperscissors":
+      perder(userChoice, computerChoice);
+    // console.log("PLAYER LOSES!")
+    case "rockrock":
+    case "paperpaper":
+    case "scissorsscissors":
+      empate(userChoice, computerChoice);
+    // console.log("DRAW!")
+  }
+
+  //   console.log(`player choice: ${userChoice}`);
+  //   console.log(`computer choice: ${computerChoice}`);
+}
+
+//rpsGame("rock");  //chckpoint output playerchoice and computerchoice
+
+//2. Make event listeners for selecting RPS choice with CLICK, decided to define fxn rps game above
+// Make rock clickable
+rockDiv.addEventListener("click", function () {
+  rpsGame("rock");
+  addPlayerChoiceImg("rock");
+  //   console.log("hey you clicked on rock");
+});
+
+// make paper clickable
+paperDiv.addEventListener("click", function () {
+  rpsGame("paper");
+  addPlayerChoiceImg("paper");
+  //   console.log("hey you clicked on paper");
+});
+
+// make scissors clickable
+scissorsDiv.addEventListener("click", function () {
+  rpsGame("scissors");
+  addPlayerChoiceImg("scissors");
+  //   console.log("hey you clicked on scissors")
+});
+
+//5. fxn that stores corresponding img to Player Choice so that it shows up in the results second
 function addPlayerChoiceImg(choice) {
   if (choice === "rock") {
     const img = document.createElement("img");
@@ -33,10 +109,12 @@ function addPlayerChoiceImg(choice) {
     const img = document.createElement("img");
     img.src = "images/cutescissors.png";
     playerChoiceImg.appendChild(img);
+  } else {
+    return;
   }
 }
 
-// fxn that stores corresponding img to playerchoice
+//6. fxn that stores corresponding img to Computer Choice so that it shows up in the results second
 function addComputerChoiceImg(choice) {
   if (choice === "rock") {
     const img = document.createElement("img");
@@ -52,31 +130,3 @@ function addComputerChoiceImg(choice) {
     computerChoiceImg.appendChild(img);
   }
 }
-
-// create game fxn that carries out RPS play
-function rpsGame(userChoice) {
-  const computerChoice = getComputerChoice();
-  addComputerChoiceImg(computerChoice);
-  addPlayerChoiceImg(userChoice);
-  console.log(`player choice: ${userChoice}`);
-  console.log(`computer choice: ${computerChoice}`);
-}
-//rpsGame("rock");  //chckpoint output playerchoice and computerchoice
-
-// Make rock clickable
-rockDiv.addEventListener("click", function () {
-  rpsGame("rock");
-  //   console.log("hey you clicked on rock");
-});
-
-// make paper clickable
-paperDiv.addEventListener("click", function () {
-  rpsGame("paper");
-  //   console.log("hey you clicked on paper");
-});
-
-// make scissors clickable
-scissorsDiv.addEventListener("click", function () {
-  rpsGame("scissors");
-  //   console.log("hey you clicked on scissors")
-});
